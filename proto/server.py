e7daf894f41aabc95c943ddbc01758aa73bfef5a
Oliver Ye <oliverye@berkeley.edu>
2022-09-13 14:40:17 -0700

    images done

--- proto/server.py
+++ proto/server.py
@@ -4,9 +4,10 @@ import image_pb2_grpc as pb_grpc
 
 from concurrent import futures
 
+
 class NLImageServiceServicer(pb_grpc.NLImageServiceServicer):
   def RotateImage(self, request, context):
-	#rotates images by 90
+	# rotates images by 90
     width = request.image.width
     height = request.image.height
     vals = request.image.bytes
@@ -23,12 +24,12 @@ class NLImageServiceServicer(pb_grpc.NLImageServiceServicer):
 	elif (request.rotation == pb_grpc.NLImageRotateRequest.Rotation.TWO_SEVENTY_DEG):
 		temp = RotateNinety(width, height, vals, pos, temp, color, 3)
 	else:
-		##TODO 
-		## THROW SOME SORT OF ERROR, INVALID REQUEST
-	response = pb.NLImage(  color=request.image.color,
+		# TODO
+		# THROW SOME SORT OF ERROR, INVALID REQUEST
+	response = pb.NLImage(color=request.image.color,
 							data=temp,
 							width=request.image.width,
-							height=request.image.height)	
+							height=request.image.height)
 	return response
 
   def RotateNinety(self, width, height, vals, pos, temp, color, times):
@@ -64,49 +65,78 @@ class NLImageServiceServicer(pb_grpc.NLImageServiceServicer):
 	count = 0
 	if (color):
 	# RGB, 3 channels
+		res1 = meanRGB(data, width, height, 0)
+		res2 = meanRGB(data, width, height, 1)
+		res3 = meanRGB(data, width, height, 2)
+		res = []
+		# res1 represents the R mean matrix, res2 represents G mean matrix, res3 represents B mean matrix
+		# here we interleave the R, G, B matrices to reconstruct the overall mean filtered matrix
+		for i in range(len(res1)):
+			res.append(res1[i])
+			res.append(res2[i])
+			res.append(res3[i])
 	else:
 	# Grayscale
-		temp = [[0] * (width + 2) for i in range(height + 2)]
-        temp2 = [[0] * (width) for i in range(height + 1)]
-        for i in range(len(l)):
-			# convert 1d row major into 2d array for easier processing
-            row = i // width
-            col = i % width
-            temp2[row + 1][col] = l[i]
-
-        # pad outside with 0s to make easier readability
-        for i in range(height + 1):
-            temp2[i].insert(0,0)
-            temp2[i].insert(width + 1,0)
-        temp2.append([0] * (width + 2))
-
-        for i in range(1, len(temp2) - 1):
-            for j in range(1, len(temp2[0]) - 1):
-                mean = 0
-                div = 9
-				# sum all 9 values, change divisor appropriately if on the edge
-                if (((i - 1 <= 0) and (j - 1 <= 0)) or
-                    ((i - 1 <= 0) and (j + 1 >= len(temp[0]) - 1)) or
-                    ((i + 1 >= len(temp) - 1) and (j - 1 <= 0)) or
-                    ((i + 1 >= len(temp) - 1) and (j + 1 >= len(temp[0]) - 1))):
-                    div = 4
-                elif ((i - 1 <= 0) or (j - 1 <= 0) or (i + 1 >= len(temp) - 1) or
-                      (j + 1 >= len(temp[0]) - 1)):
-                    div = 6
-                mean += temp2[i - 1][j - 1] + temp2[i - 1][j] + temp2[i - 1][j + 1]
-                mean += temp2[i][j - 1] + temp2[i][j] + temp2[i][j + 1]
-                mean += temp2[i + 1][j - 1] + temp2[i + 1][j] + temp2[i + 1][j + 1]
-                temp[i][j] = mean/div
-        del temp[0]
-        del temp[len(temp) - 1]
-        res = []
-        for i in range(len(temp)):
-            for j in range(1, len(temp[0]) - 1):
-				# TODO cast int type to bytes?
-                res.append(temp[i][j])
+		res = meanGrayScale(data, width, height)
 	response = pb.NLImage(color=color, data=res, width=width, height=height)
 
     return response
+
+  def meanRGB(l, width, height, offset):
+    temp = [[0] * (width + 2) for i in range(height + 2)]
+    temp2 = [[0] * (width) for i in range(height + 1)]
+    for i in range(offset, len(l), 3):
+        # convert 1d row major into 2d array for easier processing
+        row = (i//3) // width
+        col = (i//3) % width
+        temp2[row + 1][col] = l[i]
+        print(str(row) + "  " + str(col))
+	return meanFilter(temp, temp2)
+
+  def meanGrayScale(l, width, height):
+    temp = [[0] * (width + 2) for i in range(height + 2)]
+    temp2 = [[0] * (width) for i in range(height + 1)]
+    for i in range(len(l)):
+        # convert 1d row major into 2d array for easier processing
+        row = i // width
+        col = i % width
+        temp2[row + 1][col] = l[i]
+	return meanFilter(temp, temp2)
+
+  def meanFilter(temp, temp2):
+    # pad with 0s
+    for i in range(height + 1):
+        temp2[i].insert(0,0)
+        temp2[i].insert(width + 1,0)
+    temp2.append([0] * (width + 2))
+
+    for i in range(1, len(temp2) - 1):
+        for j in range(1, len(temp2[0]) - 1):
+            mean = 0
+            div = 9
+            if (((i - 1 <= 0) and (j - 1 <= 0)) or
+                ((i - 1 <= 0) and (j + 1 >= len(temp[0]) - 1)) or
+                ((i + 1 >= len(temp) - 1) and (j - 1 <= 0)) or
+                ((i + 1 >= len(temp) - 1) and (j + 1 >= len(temp[0]) - 1))):
+                div = 4
+            elif ((i - 1 <= 0) or (j - 1 <= 0) or (i + 1 >= len(temp) - 1) or
+                  (j + 1 >= len(temp[0]) - 1)):
+                div = 6
+            # index in the center
+            mean += temp2[i - 1][j - 1] + temp2[i - 1][j] + temp2[i - 1][j + 1]
+            mean += temp2[i][j - 1] + temp2[i][j] + temp2[i][j + 1]
+            mean += temp2[i + 1][j - 1] + temp2[i + 1][j] + temp2[i + 1][j + 1]
+            temp[i][j] = mean/div
+    del temp[0]
+    del temp[len(temp) - 1]
+    res = []
+    for i in range(len(temp)):
+        for j in range(1, len(temp[0]) - 1):
+            res.append(temp[i][j])
+	return res
+
+
+    
 
 def serve():
   server = grpc.server(future.ThreadPoolExecutor(max_workers=10))
