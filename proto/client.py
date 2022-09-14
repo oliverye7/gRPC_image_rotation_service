451bba3b2a1ca970c70b9865ec0e3c6bc860424e
Oliver Ye <oliverye@berkeley.edu>
2022-09-13 19:32:31 -0700

    image reading and writing complete

--- proto/client.py
+++ proto/client.py
@@ -1,8 +1,10 @@
 import argparse
+import io
 #import grpc
 #import image_pb2 as pb
 #import image_pb2_grpc as pg_grpc
 #
+from PIL import Image
 #with grpc.insecure_channel("localhost:8080") as ch:
 #  stub = pb_grpc.NLImageServiceStub(ch)
 
@@ -18,8 +20,8 @@ if __name__ == '__main__':
   #parser.add_argument('num2', help="Some sample help message here for n2", type=float)
   #parser.add_argument('--operation', help="provide operator", default = '+')
 
-  parser.add_argument('-p', '--port', help="specify port. Default port is 8080", default="8080")
-  parser.add_argument('-t', '--host', help="specify host")
+  #parser.add_argument('-p', '--port', help="specify port. Default port is 8080", default="8080")
+  #parser.add_argument('-t', '--host', help="specify host")
   parser.add_argument('-i', '--input', help="specify path to input file (jpeg or png)")
   parser.add_argument('-o', '--output', help="specify path to output folder")
   parser.add_argument('-r', '--rotate', help="specify degrees of rotation -- options are: NONE, NINETY_DEG, ONE_EIGHTY_DEG, TWO_SEVENTY_DEG. Default rotation if not provided is NONE", default="NONE")
@@ -42,3 +44,25 @@ if __name__ == '__main__':
   #if (args.operation == 'pow'):
   #    result = pow(args.num1, args.num2)
   #print("Result: "  + str(result))
+
+  im = Image.open(args.input, mode='r')
+  im_byte_arr = io.BytesIO()
+  im.save(im_byte_arr, format="PNG")
+  im_byte_arr = im_byte_arr.getvalue()
+  print(type(im_byte_arr))
+
+  filename = args.input.split("/")
+  filename = filename[len(filename) - 1]
+  print(filename)
+
+  filename = filename.split(".")
+  filename.insert(1, ".")
+  if (args.rotate != 'NONE'):
+    filename.insert(1, "_Rotated")
+  if (args.mean):
+    filename.insert(1, "_Meaned")
+  output = ''.join(filename)
+  print(output)
+  f = open(output, "wb")
+  f.write(im_byte_arr)
+  f.close()
