d402a9bb3a723f0504d9546aa38f4f2ec2dea450
Oliver Ye <oliverye@berkeley.edu>
2022-09-14 13:47:46 -0700

    image parsing set up

--- proto/client.py
+++ proto/client.py
@@ -1,13 +1,21 @@
 import argparse
+import cv2
 import io
 #import grpc
 #import image_pb2 as pb
 #import image_pb2_grpc as pg_grpc
 #
-from PIL import Image
+from PIL import Image, ImageOps
 #with grpc.insecure_channel("localhost:8080") as ch:
 #  stub = pb_grpc.NLImageServiceStub(ch)
 
+def isgray(imgpath):
+    img = cv2.imread(imgpath)
+    if len(img.shape) < 3: return True
+    if img.shape[2]  == 1: return True
+    b,g,r = img[:,:,0], img[:,:,1], img[:,:,2]
+    if (b==g).all() and (b==r).all(): return True
+    return False
 
 if __name__ == '__main__':
   # initialize
@@ -16,10 +24,6 @@ if __name__ == '__main__':
   )
 
   # parameters (positional/optional ones)
-  #parser.add_argument('num1', help="Some sample help message here for n1", type=float)
-  #parser.add_argument('num2', help="Some sample help message here for n2", type=float)
-  #parser.add_argument('--operation', help="provide operator", default = '+')
-
   #parser.add_argument('-p', '--port', help="specify port. Default port is 8080", default="8080")
   #parser.add_argument('-t', '--host', help="specify host")
   parser.add_argument('-i', '--input', help="specify path to input file (jpeg or png)")
@@ -32,24 +36,44 @@ if __name__ == '__main__':
   args = parser.parse_args()
   print()
   print(args)
-  result = None
-  #if (args.operation == '+'):
-  #    result = args.num1 + args.num2
-  #if (args.operation == '-'):
-  #    result = args.num1 - args.num2
-  #if (args.operation == '/'):
-  #    result = args.num1 / args.num2
-  #if (args.operation == '*'):
-  #    result = args.num1 * args.num2
-  #if (args.operation == 'pow'):
-  #    result = pow(args.num1, args.num2)
-  #print("Result: "  + str(result))
+
+  isgray = isgray(args.input)
+  print(isgray)
 
   im = Image.open(args.input, mode='r')
   im_byte_arr = io.BytesIO()
   im.save(im_byte_arr, format="PNG")
   im_byte_arr = im_byte_arr.getvalue()
   print(type(im_byte_arr))
+  #print(im_byte_arr)
+  pix_val = list(im.getdata())
+  #print(pix_val)
+
+  # get width and height
+  width = im.width
+  height = im.height
+	
+  print(width)
+  print(height)
+  print(width * height)
+  if (isgray):
+    grayVals = []
+    for i in range(len(pix_val)):
+        grayVals.append((pix_val[i][0]).to_bytes(1, byteorder='big'))
+    print(len(grayVals))
+    #print(grayVals)
+  else:
+    print(len(pix_val))
+    pix_val_flat = [x for sets in pix_val for x in sets]
+    del pix_val_flat[4-1::4]
+    print(len(pix_val_flat))
+    #print(pix_val_flat)
+
+
+
+
+
+
 
   filename = args.input.split("/")
   filename = filename[len(filename) - 1]
@@ -63,6 +87,9 @@ if __name__ == '__main__':
     filename.insert(1, "_Meaned")
   output = ''.join(filename)
   print(output)
-  f = open(output, "wb")
+
+
+
+  f = open(args.output + "/" + output, "wb")
   f.write(im_byte_arr)
   f.close()
